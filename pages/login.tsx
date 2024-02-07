3a3176ce156bad636d60617618ae93fdb3dd74e2
lazywk <edoniyorbekdev@gmail.com>
2024-02-07 16:11:29 +0500

    dropdown

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,15 +1,8 @@
 import LoginLayout from '@/components/layout/login-layout'
-import Input, { Label } from '@/components/ui/input'
 import React from 'react'
 
 export default function Login() {
     return (
-        <LoginLayout>
-            <div className="flex flex-col gap-4">
-                <Label label='Position'>
-                    <Input placeholder='UI/UX Designer' />
-                </Label>
-            </div>
-        </LoginLayout>
+        <LoginLayout>login</LoginLayout>
     )
 }
